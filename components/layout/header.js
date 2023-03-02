6eb53e498695cc70e53970fd7e034dff116b3028
Alexander ₿ <roby.sw@proton.me>
2023-03-02 02:14:48 +0100

    Fixed Header Popover

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Fragment, useState } from "react";
+import { Fragment, useRef, useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
@@ -36,6 +36,8 @@ export default function Header() {
   const pathname = usePathname();
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const utilitiesRef = useRef();
 
   return (
     <header className="bg-kt-black">
@@ -76,6 +78,7 @@ export default function Header() {
           </Link>
           <Popover className="relative">
             <Popover.Button
+              ref={utilitiesRef}
               className={
                 `flex items-center gap-x-1 text-sm font-semibold leading-6 hover:text-gray-50 focus:outline-none ` +
                 (pathname === "/position-size-calculator"
@@ -102,14 +105,14 @@ export default function Header() {
               <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-md ring-1 ring-kt-black/5">
                 <div className="p-4">
                   {utilities.map((item) => (
-                    <Popover.Button
-                      as={Link}
+                    <Link
                       href={item.href}
                       key={item.name}
                       className={
                         `group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6  ` +
                         (pathname === "/" ? "hover:bg-gray-50" : "bg-gray-50")
                       }
+                      onClick={() => utilitiesRef.current?.click()}
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-kt-yellow">
                         <item.icon
@@ -124,23 +127,23 @@ export default function Header() {
                         </div>
                         <p className="mt-1 text-gray-500">{item.description}</p>
                       </div>
-                    </Popover.Button>
+                    </Link>
                   ))}
                 </div>
                 <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
                   {callsToAction.map((item) => (
-                    <Popover.Button
-                      as="a"
+                    <a
                       key={item.name}
                       href={item.href}
                       className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                      onClick={() => utilitiesRef.current?.click()}
                     >
                       <item.icon
                         className="h-5 w-5 flex-none text-gray-400"
                         aria-hidden="true"
                       />
                       {item.name}
-                    </Popover.Button>
+                    </a>
                   ))}
                 </div>
               </Popover.Panel>
@@ -222,15 +225,14 @@ export default function Header() {
 
                       <Disclosure.Panel className="mt-2 space-y-2">
                         {[...utilities /*, ...callsToAction*/].map((item) => (
-                          <Disclosure.Button
+                          <Link
                             key={item.name}
-                            as={Link}
                             href={item.href}
                             onClick={() => setMobileMenuOpen(false)}
                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                           >
                             {item.name}
-                          </Disclosure.Button>
+                          </Link>
                         ))}
                       </Disclosure.Panel>
                     </>
